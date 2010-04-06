5e2340decd358d2405a1347847e92804a4745bf4
Harry Brundage <harry@skylightlabs.ca>
2010-04-06 16:15:17 -0400

    Fixing dashboard syntax error and application layout if statement fudge up

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,12 +1,13 @@
 class DashboardController < ApplicationController
+  before_filter :require_user
 	def index
-		@activity = Business.find(:all)
-		@activity += Invoice.find(:all)
-		@activity += Project.find(:all)
-		@activity += Expense.find(:all)
-		@activity += HourReport.find(:all)
-		@activity += User.find(:all)
-		@activity += Task.find(:all)
+	  sql = {:order => "updated_at DESC", :limit => 10}
+		@activity = Business.find(:all, sql)
+		@activity += Invoice.find(:all, sql)
+		@activity += Project.find(:all, sql)
+		@activity += ProjectCost.find(:all, sql)
+		@activity += User.find(:all, sql)
+		@activity += Task.find(:all, sql)
 
 		@activity = @activity.sort { |x, y| 	y.created_at <=> x.created_at }
 	
