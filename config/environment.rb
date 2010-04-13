5255be320ef5385d30835ae7f7f015d238fe2299
Harry Brundage <harry@skylightlabs.ca>
2010-04-13 17:22:00 -0400

    heroku fiddling

--- config/environment.rb
+++ config/environment.rb
@@ -20,7 +20,6 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   
-  config.gem "sqlite3-ruby", :lib => "sqlite3"
   config.gem "authlogic"
   config.gem "carmen"
   config.gem "simple_autocomplete"
