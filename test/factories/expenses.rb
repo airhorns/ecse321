59f5e14f6840801d00987b16977be18fa4fd3048
Henry <henry@ubuntu.ubuntu-domain>
2010-04-13 18:59:56 -0400

    some tests

--- test/factories/expenses.rb
+++ test/factories/expenses.rb
@@ -5,3 +5,39 @@ Factory.define :expense do |e|
   e.association :user
   e.association :task
 end
+
+Factory.define :pending do |e|
+  e.cost 10
+  e.sequence(:name) {|n| "Expense ##{n}" }
+  e.description "Something that cost money."
+  e.association :user
+  e.association :task
+  e.state = Expense::Pending
+end
+
+Factory.define :approved do |e|
+  e.cost 10
+  e.sequence(:name) {|n| "Expense ##{n}" }
+  e.description "Something that cost money."
+  e.association :user
+  e.association :task
+  e.state = Expense::Approved
+end
+
+Factory.define :rejected do |e|
+  e.cost 10
+  e.sequence(:name) {|n| "Expense ##{n}" }
+  e.description "Something that cost money."
+  e.association :user
+  e.association :task
+  e.state = Expense::Rejected
+end
+
+Factory.define :invoiced do |e|
+  e.cost 10
+  e.sequence(:name) {|n| "Expense ##{n}" }
+  e.description "Something that cost money."
+  e.association :user
+  e.association :task
+  e.state = Expense::Invoiced
+end
