193a5e4b828e357afaf1e195e9a88423f66d61fe
Nicholas Sabatino <me@nicksabatino.com>
2010-04-14 20:59:51 -0400

    Adding project and task unit tests

--- test/unit/project_test.rb
+++ test/unit/project_test.rb
@@ -1,8 +1,29 @@
 require 'test_helper'
 
 class ProjectTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  context "Projects" do
+    setup do
+			@project = Factory.create(:project)
+
+      @task1 = Factory.create(:task, :project => @project)
+      @expense1 = Factory.create(:expense, :task => @task1)
+      @hour_report1 = Factory.create(:hour_report, :task => @task1)
+
+      @task2 = Factory.create(:task, :project => @project)
+      @expense2 = Factory.create(:expense, :task => @task2)
+      @hour_report2 = Factory.create(:hour_report, :task => @task2)
+    end
+
+    should "should have tasks" do
+			assert @task1.project == @project
+			assert @task2.project == @project
+    end
+
+    should "properly calculate the cost" do
+			cost = @task1.get_cost + @task2.get_cost
+
+			assert_equal @project.get_cost, cost
+    end
   end
 end
--- test/unit/task_test.rb
+++ test/unit/task_test.rb
@@ -1,8 +1,23 @@
 require 'test_helper'
 
-class TaskTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+class HourReportTest < ActiveSupport::TestCase
+
+  context "Tasks" do
+    setup do
+      @task = Factory.create(:task)
+      @expense = Factory.create(:expense, :task => @task)
+      @hour_report = Factory.create(:hour_report, :task => @task)
+    end
+
+    should "should have project costs" do
+			assert @expense.task == @task
+			assert @hour_report.task == @task
+    end
+
+    should "properly calculate the cost" do
+			cost = @expense.get_cost + @hour_report.get_cost
+
+			assert_equal @task.get_cost, cost
+    end
   end
 end
